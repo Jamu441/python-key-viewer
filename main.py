85923c1d69e246914cfd0a17686da29f7156c12d
jamu441 <itsmejwroblox@gmail.com>
2024-11-21 16:28:39 -0500

    improved in many places, added config file.

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
 import ttkbootstrap as ttk
 import keyboard
 import for_dark_mode as dm
+import json
 
-window = ttk.Window(title="Key Viewer", size=(400, 100), themename="cyborg")
-window.wm_attributes("-alpha", "0.5")
+with open("config.json", "r") as file:
+    settings = json.load(file)
+
+window = ttk.Window(title="Key Viewer", size=(settings["width"], settings["height"]), themename="cyborg")
+window.wm_attributes("-alpha", settings["alpha"], "-topmost", settings["always_on_top"])
 window.position_center()
 
 dm.update_dark_mode(window=window)
 
-font_size = 18
-max_char = 28
+font = settings["font"]
+font_size = settings["font_size"]
+max_char = settings["max_char"]
 
 # functions
 def change_key_str(str):
+    dash_chars = ["ctrl", "shift", "up", "down", "left", "right"]
     if str == "space":
-        str = "-"
-    elif "control" in str:
-        str = "ctrl"
-    elif "shift" in str:
-        str = "shift"
-    elif str == "caps_lock":
+        str = "└─┘"
+    elif str in dash_chars:
+        str += "-"
+    elif str == "caps lock":
         str = "caps"
+    elif str == "backspace":
+        str = "←"
+    elif str == "enter":
+        str = "→"
     return str
 
 def on_key(event):
     if len(keys_label.cget("text")) < max_char:
-        key = event.keysym.lower()
-        key = change_key_str(key)
-        keys_label.config(text=keys_label.cget("text") + key)
+        if event.event_type == keyboard.KEY_DOWN:
+            string = str(event.name).lower() 
+            key = change_key_str(string) + settings["seperator_char"]
+            keys_label.config(text=keys_label.cget("text") + key)
     else:
         keys_label.config(text="")
 
 # widgets
-keys_label = ttk.Label(window, font=f"Consolas {font_size}")
-keys_label.pack(pady=25)
+keys_label = ttk.Label(window, font=f"{font} {font_size}", foreground=settings["text_color"])
+keys_label.pack(pady=settings["pad_y"])
 
-window.bind("<KeyPress>", on_key)
+keyboard.hook(on_key)
 
 window.mainloop()
